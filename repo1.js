369130c4cede7a4ec9c3aeb7e2c0766669bdf85a
andrewpaek <andrew.k.paek@gmail.com>
2016-04-10 15:11:24 -0400

    before pulling

---
+++ repo1.js
@@ -0,0 +1,9 @@
+var addValue = function() {
+	s = "<p id='balance'> Balance: <p> <p id='actual_amount'> What's up </p>";
+	$("div.funds").html(s);
+	k = "What's up";
+	$("div.hello").html(k);
+	console.log("hello world");
+}
+
+window.onload=init;
