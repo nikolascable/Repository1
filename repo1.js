aa365a95d98b48031bef3a47e8bfb8da64e299f6
andrewpaek <andrew.k.paek@gmail.com>
2016-04-12 15:19:43 -0400

    pulling before push

--- repo1.js
+++ repo1.js
@@ -1,29 +1,37 @@
+var stored={"balance": 0};
+
 var addValue = function() {
-	s = "<p id='balance'> Balance: <p> <p id='actual_amount'> What's up </p>";
-	$("div.funds").html(s);
-	// k = "What's up";
-	// $("div.hello").html(k);
-	console.log("hello world");
-	var divs = document.getElementById("content").innerHTML;
-	var storeds={"html": divs}
-	var tostore=JSON.stringify(storeds);
+	var add_amt = parseInt($("#amount_input").val());
+	var stored_temp = localStorage.getItem("stored");
+	if (stored_temp != null) {
+		stored=JSON.parse(stored_temp);
+	}
+	console.log(add_amt);
+	console.log(stored);
+	var new_balance=stored.balance+add_amt;
+	//storing again
+	stored.balance=new_balance;
+	var tostore=JSON.stringify(stored);
 	localStorage.setItem("stored", tostore);
+
 }
+
+
 
 var init = function() {
-	var stored=localStorage.getItem("stored");
-	if (stored != null) {
-		console.log("hheeeeelllloooo")
-		var storeds=JSON.parse(stored);
-		console.log(storeds);
-		$('#content').html(storeds.html);
-		console.log("hello world");
-	};
+	var temp = localStorage.getItem("stored");
+	var new_balance=0;
+	if (temp != null){
+		var stored = JSON.parse(temp);
+		new_balance=stored.balance;
+	}
+
+	var s3 = "$"+new_balance;
+	$(".funds #actual_amount").html(s3);
+
+	var s1 = "Balance: $";
+	var s4 = s1+new_balance;
+	$(".card h1").text(s4);
 }
 
-window.onload=init;
-
-// What's the best way to work with different html pages? One javascript function 
-// is being called form one page and affects another. Online source says to use
-// iframes but it seems messy. Also, our user html page seems to reload every time
-// we call the function and our changes to the html revert back
+$(document).ready(init);
