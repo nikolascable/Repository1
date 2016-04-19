4a48e4a28af0441f15cd62a266f5650d73db7f25
sariyasiddiqui <sariyasiddiqui@Sariyas-MacBook-Pro.local>
2016-04-19 14:18:51 -0400

    store transit system to local storage

--- repo1.js
+++ repo1.js
@@ -16,7 +16,21 @@ var addValue = function() {
 
 }
 
+//var select = document.querySelector("select");
+//var select = $("#myselect :selected").text();
 
+//var system = {"sys": ''}
+//var changeSystem = function() {
+//    var select = document.querySelector(".testSelect");
+//    var selectOption = select.options[select.selectedIndex].text;
+//    var new_system = $("#myselect :selected").text();
+//    var stored_system = localStorage.getItem("system");
+//    if (stored_system != null){
+//        system = JSON.parse(stored_system);
+//    }
+//    system.sys = new_system;
+//    localStorage.setItem("system", JSON.stringify(system));
+//}
 
 var init = function() {
 	var temp = localStorage.getItem("stored");
@@ -32,6 +46,32 @@ var init = function() {
 	var s1 = "Balance: $";
 	var s4 = s1+new_balance;
 	$(".card h1").text(s4);
+    
+//    var sys = localStorage.getItem("system");
+//    var new_system = '';
+//    if (system != null){
+//        var system = JSON.parse(sys);
+//        new_system = system.sys;
+//    }
+//    $("#value").text(new_system);
+    
+    var select = document.querySelector("#myselect");
+    console.log(select);
+    var selectOption = select.options[select.selectedIndex];
+    var lastSelected = localStorage.getItem('select');
+
+    if(lastSelected) {
+        select.value = lastSelected;
+        $("#value").text(lastSelected);
+    }
+
+    select.onchange = function () {
+       lastSelected = select.options[select.selectedIndex].text;
+        $("#value").text(lastSelected);
+       console.log(lastSelected);
+       localStorage.setItem('select', lastSelected);
+    }
+
 }
 
 $(document).ready(init);
